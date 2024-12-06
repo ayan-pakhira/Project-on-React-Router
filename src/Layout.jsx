b9b11c0ccc485e0da8ca92e55dfabcc978fe4274
ayan3018 <ayan.pakhira3018@gmail.com>
2024-12-06 14:55:40 +0530

    project partially done

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
